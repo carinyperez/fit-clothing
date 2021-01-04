084a965056661bdadfffed9c7bd8cb63f22fdd8d
Cariny Perez <perezcariny@gmail.com>
2021-01-04 15:30:31 -0800

    add files for homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react'; 
+import './homepage.styles.scss'; 
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>BOTTOMS AND LEGGINGS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HOODIES AND JACKETS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>SHORTS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>SPORTS BRAS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>TSHIRTS AND TOPS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+); 
+
+export default HomePage; 
