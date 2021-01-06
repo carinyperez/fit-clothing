eff1984df76fe2de1f56db321167664cc5c5802b
Cariny Perez <perezcariny@gmail.com>
2021-01-06 12:40:54 -0800

    update our menu-items to navigate using withRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
-
+import {Switch, Route} from 'react-router-dom'; 
 import HomePage from './pages/homepage/homepage.component'; 
+
+const BottomsAndLeggingsPage = () => (
+  <div>
+    <h1>BOTTOMS AND LEGGINGS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/bottomsandleggings' component={BottomsAndLeggingsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,41 +11,47 @@ class Directory extends React.Component {
                 title: 'bottoms and leggings',
                 imageUrl: 'https://cdn.shopify.com/s/files/1/1812/3759/products/PowerLeggings_Lilac_06_1200x.jpg?v=1598260946',
                 size: 'small',
-                id: 1
+                id: 1,
+                linkUrl: 'bottomsandleggings'
             },
             {
                 title: 'hoodies and jackets',
-                imageUrl: 'https://cdn.shopify.com/s/files/1/1812/3759/products/CroppedHoodie_RoyalBlue_05_a0b03f12-49f4-400c-b280-3aede181e6f6.jpg?v=1606820978',
+                imageUrl: 'https://cdn.shopify.com/s/files/1/1812/3759/products/CroppedHoodie_RoyalBlue_01_64c25d34-94f6-4b35-ac0a-840554d043bf_650x.jpg?v=1606820976',
                 size: 'small',
-                id: 2
+                id: 2,
+                linkUrl: ''
             },
             {
                 title: 'shorts',
                 imageUrl: 'https://cdn.shopify.com/s/files/1/1812/3759/products/CyclingShorts_Red_03_200x200_crop_center@2x.jpg?v=1598260938',
                 size: 'small', 
-                id: 3
+                id: 3,
+                linkUrl: ''
             },
             {
                 title: 'sports bras',
                 imageUrl: 'https://cdn.shopify.com/s/files/1/1812/3759/products/PowerBra_Black_02.jpg?v=1557241432',
                 size: 'large',
-                id: 4
+                id: 4,
+                linkUrl: ''
             },
             {
                 title: 't-shirts and tops',
                 imageUrl: 'https://cdn.shopify.com/s/files/1/1812/3759/products/PowerLongSleeve_Black_02.jpg?v=1557241362',
                 size: 'large',
-                id: 5
+                id: 5,
+                linkUrl: ''
             }
             ]
         }
     }
 
-    render() {
-        return (
+    render() { 
+        return ( 
             <div className='directory-menu'>
-                {this.state.sections.map(({ title, imageUrl, id, size }) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                {this.state.sections.map(({id, ...otherSectionProps }) => (
+                    <MenuItem key={id} {...otherSectionProps}/>
+                    
                  )) 
                 }  
             </div>
