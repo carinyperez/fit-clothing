e1c379b2c3bbd7eb9f7f43af79fac181baf6b364
Cariny Perez <perezcariny@gmail.com>
2021-01-26 19:04:24 -0800

    Adding stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout'; 
+import './stripe-button.styles.scss';
+import logo from '../../assets/wellness-logo.svg'; 
+
+
+const StripeCheckoutButton = ({price}) => {
+    const priceForStripe = price * 100; 
+    const publishableKey = 'pk_test_51IE26sGXsFzuFS5CYkPQQAxau71iyBuoetfr3SXdnA1PNHvkQfyR7w9DaES9K4iVyhaIKN1d6yQMp4VJloll4LeL00gBOohKcP';
+    
+    const onToken = token => {
+        console.log(token); 
+        alert('Payment Successful'); 
+    }
+
+    return(
+        <StripeCheckout 
+            label='Pay Now'
+            name="Fit Clothing"
+            billingAddress 
+            shippingAddress
+            image={logo}
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        /> 
+    )
+}
+
+export default StripeCheckoutButton; 
